a4e099501d57090b8599229218036c6bf01caa5e
Roman Stravnyak <stravnyakrv@tech.bcs.ru>
2026-06-17 18:22:43 +0700

    update configs

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,27 +1,23 @@
-require('dotenv').config({ path: '.env.deploy' });
+const path = require('path');
+
+require('dotenv').config({ path: path.join(__dirname, '..', '.env.deploy') });
 
 const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO,
 } = process.env;
 
 module.exports = {
-  apps: [{
-    name: 'backend',
-    script: './dist/app.js',
-    env: {
-      NODE_ENV: 'production',
-    },
-  }],
-
+  apps: [{ name: 'backend', script: './dist/app.js' }],
   deploy: {
     production: {
       user: DEPLOY_USER,
-      host: [DEPLOY_HOST],
+      host: DEPLOY_HOST,
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'npm install && npm run build',
+      'pre-deploy': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend`,
+      'post-deploy':
+        'npm ci --omit=dev && npm run build && pm2 reload ecosystem.config.js --env production',
     },
   },
 };
--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,29 +1,20 @@
-require('dotenv').config({ path: '.env.deploy' });
+const path = require('path');
+
+require('dotenv').config({ path: path.join(__dirname, '..', '.env.deploy') });
 
 const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO,
 } = process.env;
 
 module.exports = {
-  apps: [{
-    name: 'frontend',
-    script: 'node_modules/react-scripts/scripts/start.js',
-    env: {
-      NODE_ENV: 'production',
-    },
-  }],
-
   deploy: {
     production: {
-      key: '~/.ssh/deploy.key',
       user: DEPLOY_USER,
-      host: [DEPLOY_HOST],
-      ssh_options: 'StrictHostKeyChecking=no',
+      host: DEPLOY_HOST,
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'post-setup': 'npm run build',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js --env production',
+      'post-deploy': 'npm ci && npm run build',
     },
   },
 };
