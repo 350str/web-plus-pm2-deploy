86756bc2850ca91711b54ccff183be0fa66b92de
Roman Stravnyak <stravnyakrv@tech.bcs.ru>
2026-06-17 19:33:03 +0700

    updates

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,6 +1,6 @@
-const path = require('path');
+const dotenv = require('dotenv');
 
-require('dotenv').config({ path: path.join(__dirname, '..', '.env.deploy') });
+dotenv.config({ path: './.env.deploy' });
 
 const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO,
@@ -14,10 +14,11 @@ module.exports = {
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
-      path: `${DEPLOY_PATH}/backend`,
-      'pre-deploy': `scp ${__dirname}/.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/backend/current/backend`,
+      path: `${DEPLOY_PATH}`,
+      ssh_options: 'StrictHostKeyChecking=no',
+      'pre-deploy-local': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy':
-        '. "$HOME/.nvm/nvm.sh" 2>/dev/null || true; npm ci --omit=dev && npm run build && pm2 reload ecosystem.config.js --env production',
+        'cd backend && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };
--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,21 +1,24 @@
-const path = require('path');
-
-require('dotenv').config({ path: path.join(__dirname, '..', '.env.deploy') });
+const dontenv = require("dotenv");
+dontenv.config({ path: "./.env.deploy" });
 
 const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO,
 } = process.env;
 
 module.exports = {
+  apps: [
+    {
+      name: "frontend",
+    },
+  ],
   deploy: {
     production: {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
-      path: `${DEPLOY_PATH}/frontend`,
-      'post-deploy':
-        '. "$HOME/.nvm/nvm.sh" 2>/dev/null || true; npm ci && npm run build',
+      path: `${DEPLOY_PATH}`,
+      "post-deploy": "pwd && cd frontend && npm ci && npm run build",
     },
   },
 };
