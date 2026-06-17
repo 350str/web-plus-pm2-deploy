2c8d49624ddd3b41f39b80795e34ee3b262a2aec
Roman Stravnyak <stravnyakrv@tech.bcs.ru>
2026-06-17 22:09:21 +0700

    updates

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,10 +13,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 mongoose.connect(DB_ADDRESS);
 
-app.use(cors({
-  origin: 'https://mesto.350str.nomorepartiessite.ru',
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
