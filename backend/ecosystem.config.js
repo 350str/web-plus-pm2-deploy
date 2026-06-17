9d5dbffff3fa1a8de828980c1bf0db5582de073d
Roman Stravnyak <stravnyakrv@tech.bcs.ru>
2026-06-17 19:50:23 +0700

    updates

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './.env.deploy' });
+dotenv.config({ path: '.env.deploy' });
 
 const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO,
@@ -16,9 +16,11 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: `${DEPLOY_PATH}`,
       ssh_options: 'StrictHostKeyChecking=no',
-      'pre-deploy-local': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy-local': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
       'post-deploy':
-        'export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh" && cd backend && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+        'cd backend && npm i && npm run build && npx pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 };
+
+// export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh" &&
