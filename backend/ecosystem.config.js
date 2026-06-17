2c92d53bb9132076312e31e7645d7e5357f2c32d
Roman Stravnyak <stravnyakrv@tech.bcs.ru>
2026-06-17 19:59:46 +0700

    updates

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -6,6 +6,8 @@ const {
   DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO,
 } = process.env;
 
+const NVM_INIT = 'export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"';
+
 module.exports = {
   apps: [{ name: 'backend', script: './dist/app.js' }],
   deploy: {
@@ -16,11 +18,10 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: `${DEPLOY_PATH}`,
       ssh_options: 'StrictHostKeyChecking=no',
+      'pre-setup': `rm -rf ${DEPLOY_PATH}`,
       'pre-deploy-local': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
       'post-deploy':
-        'cd backend && npm i && npm run build && npx pm2 startOrRestart ecosystem.config.js --env production',
+        `${NVM_INIT} && cd backend && npm i && npm run build && npx pm2 startOrRestart ecosystem.config.js --env production`,
     },
   },
 };
-
-// export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh" &&
